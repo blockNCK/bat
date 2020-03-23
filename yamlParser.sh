165d136cafb55bd03eb17344197f91ed474a424b
Chathurangi Edussuriya <edussuriyac@gmail.com>
2020-03-23 09:15:17 +0530

    add yaml parser

--- yamlParser.sh
+++ yamlParser.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 function parse_yaml {
-   local prefix=$2
-   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   prefix=$2
+   s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
    sed -ne "s|^\($s\):|\1|" \
         -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
         -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
