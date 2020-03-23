1c7951dd6978a2f29c7d4f4898ccc0a0784dcb56
Chathurangi Edussuriya <edussuriyac@gmail.com>
2020-03-23 08:55:15 +0530

    add yaml parser

--- bat.sh
+++ bat.sh
@@ -9,3 +9,13 @@ echo "--------------------------------------------------------------------------
 echo "                                  Invoke wallet services                                            "
 echo "----------------------------------------------------------------------------------------------------"
 
+yamlParser config.yaml
+
+if database = "mongo"
+  if [[ ! $(which mongo) ]]
+  then
+  display_msg "Install mongodb\n"
+  sudo apt update
+  sudo apt install -y mongodb
+  fi
+fi
