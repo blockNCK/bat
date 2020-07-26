c99f2c7735851191523df336ba7891f66b2286eb
Chathurangi Edussuriya <edussuriyac@gmail.com>
2020-07-26 05:53:19 +0530

    modify bat script

--- yamlParser.sh
+++ yamlParser.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
- function parse_yaml {
-   local prefix=$2
-   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+ 
+   prefix=$2
+   s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
    sed -ne "s|^\($s\):|\1|" \
         -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
         -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
@@ -15,8 +15,6 @@
          printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
       }
    }'
-}
 
-parse_yaml
 
 
